501cf033d126d8ff5b3ab6f303c4a8afe55add2b
Gianni Adamo <gianni.adamo@hotmail.it>
2023-04-11 11:36:18 +0200

    Commit

--- Libraries/Container/linear.cpp
+++ Libraries/Container/linear.cpp
@@ -5,10 +5,43 @@
 
 namespace lasd {
 
-/* ************************************************************************** */
+/////////////////////////////////////////////////////////////////////// LinearContainer
 
-// ...
+// Override operator ==
+template <typename Data>
+bool LinearContainer<Data>::operator == (const LinearContainer<Data>& otherContainer) const noexcept{
 
-/* ************************************************************************** */
+    if((*this).Size() != otherContainer.Size()){
+
+        for(i=0; i<(this*).Size(); i++){
+
+            if((*this)[i] != otherContainer[i]) {return false; }
+        }
+
+        return true;
+    }
+
+    else { return false; }
+}
+
+// Override operator !=
+template <typename Data>
+bool LinearContainer<Data>::operator != (const LinearContainer<Data>&) const noexcept{
+
+    if((*this).Size() != otherContainer.Size()){
+
+        for(i=0; i<(this*).Size(); i++){
+
+            if((*this)[i] != otherContainer[i]) { return true; }
+        }
+
+        return false;
+    }
+
+    else { return true; }
+}
+
+// Override ???
+
 
 }
