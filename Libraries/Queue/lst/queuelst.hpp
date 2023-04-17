e36f22ef9d4c6a6a329670727424a55c02b18594
Gianni Adamo <gianni.adamo@hotmail.it>
2023-04-17 12:41:51 +0200

    Commit

---
+++ Libraries/Queue/lst/queuelst.hpp
@@ -0,0 +1,97 @@
+
+#ifndef QUEUELST_HPP
+#define QUEUELST_HPP
+
+/* ************************************************************************** */
+
+#include "../queue.hpp"
+#include "../../list/list.hpp"
+
+/* ************************************************************************** */
+
+namespace lasd {
+
+/* ************************************************************************** */
+
+template <typename Data>
+class QueueLst : public virtual Queue<Data>,
+                  public virtual List<Data>{
+                  // Must extend Queue<Data>,
+                  //             List<Data>
+
+private:
+
+  // ...
+
+protected:
+
+  using List<Data>::size;
+
+  // ...
+
+public:
+
+  // Default constructor
+  QueueLst() = default;
+
+  /* ************************************************************************ */
+
+  // Specific constructor #1: QueueLst obtained from a MappableContainer
+  QueueLst(const MappableContainer<Data>&) noexcept;
+
+  // Specific constructor #2: QueueLst obtained from a MutableMappableContainer
+  QueueLst(MutableMappableContainer<Data>&&) noexcept;
+
+  /* ************************************************************************ */
+
+  // Copy constructor
+  QueueLst(const QueueLst&);
+
+  // Move constructor
+  QueueLst(QueueLst&&) noexcept;
+
+  /* ************************************************************************ */
+
+  // Destructor
+  virtual ~QueueLst();
+
+  /* ************************************************************************ */
+
+  // Copy assignment
+  // type operator=(argument);
+
+  // Move assignment
+  // type operator=(argument);
+
+  /* ************************************************************************ */
+
+  // Comparison operators
+  // type operator==(argument) specifiers;
+  // type operator!=(argument) specifiers;
+
+  /* ************************************************************************ */
+
+  // Specific member functions (inherited from Queue)
+
+  // type Head() specifiers; // Override Queue member (non-mutable version; must throw std::length_error when empty)
+  // type Head() specifiers; // Override Queue member (mutable version; must throw std::length_error when empty)
+  // type Dequeue() specifiers; // Override Queue member (must throw std::length_error when empty)
+  // type HeadNDequeue() specifiers; // Override Queue member (must throw std::length_error when empty)
+  // type Enqueue(argument) specifiers; // Override Queue member (copy of the value)
+  // type Enqueue(argument) specifiers; // Override Queue member (move of the value)
+
+  /* ************************************************************************ */
+
+  // Specific member function (inherited from ClearableContainer)
+
+  // using List<Data>::Clear;
+
+};
+
+/* ************************************************************************** */
+
+}
+
+#include "queuelst.cpp"
+
+#endif
